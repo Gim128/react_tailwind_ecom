a58b2e848c7f756eb75d2f8cca6f3db41f89f730
Gim128 <gimhanpabasara4@gmail.com>
2023-12-05 22:00:15 +0530

    install material UI, material icons, google fonts.add material icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,29 @@
 // import React from 'react'
-import menu from '../../img/menu.svg'
-import search from '../../img/search.svg'
-import cart from '../../img/cart.svg'
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Header = () => {
     return (
-        <header>
+        <header className="fixed top-0 left-0 z-[100] w-full p-5 bg-my-background flex items-center justify-between drop-shadow-header-shadow">
             {/* header left */}
-            <div>
-                <img src={menu} alt="..." />
-                <h1>Leo <span>D Mall</span></h1>
-                <div>
-                    <input type="text" placeholder="iphone 15 pro max" />
+            <div className="flex items-center">
+                <MenuIcon
+                sx={{
+                    color:"black"
+                }}
+                />
+                <h1 className="ml-2 text-sm font-semibold">Leo <span className="text-[#c82196]">D Mall</span></h1>
+                <div className="overflow-hidden ml-2 flex items-center rounded-full bg-[#f1e6e6]">
+                    <input type="text" placeholder="iphone"
+                    className="outline-none p-2 font-semibold text-sm w-[200px] bg-inherit"
+                    />
+                    <SearchIcon className='text-gray-400'/>
                 </div>
-                <img src={search} alt="..." />
             </div>
 
             {/* header right */}
-            <img src={cart} alt="..." />
+            <ShoppingCartOutlinedIcon />
             {/*32:59*/}
         </header>
     )
