61ab7a0e6b56c8a27324e33b73ff11eb835ede18
Gim128 <gimhanpabasara4@gmail.com>
2023-12-05 22:00:33 +0530

    install material UI, material icons, google fonts.add material icons

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Body from "./components/Body/Body";
+
+
+function App() {
+  return (
+    <div className="w-full h-screen">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
