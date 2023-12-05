c6aced1255b4375ccc8d39e506d1cf9806bbc0ba
Gim128 <gimhanpabasara4@gmail.com>
2023-12-05 22:31:40 +0530

    Add IconButtonEffect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,28 +3,36 @@ import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
+import { IconButton } from "@mui/material";
+
 const Header = () => {
     return (
         <header className="fixed top-0 left-0 z-[100] w-full p-5 bg-my-background flex items-center justify-between drop-shadow-header-shadow">
             {/* header left */}
             <div className="flex items-center">
-                <MenuIcon
-                sx={{
-                    color:"black"
-                }}
-                />
+                <IconButton sx={{color: "red"}}>
+                    <MenuIcon
+                        sx={{
+                            color:"black"
+                        }}
+                    />
+                </IconButton>
                 <h1 className="ml-2 text-sm font-semibold">Leo <span className="text-[#c82196]">D Mall</span></h1>
                 <div className="overflow-hidden ml-2 flex items-center rounded-full bg-[#f1e6e6]">
                     <input type="text" placeholder="iphone"
                     className="outline-none p-2 font-semibold text-sm w-[200px] bg-inherit"
                     />
-                    <SearchIcon className='text-gray-400'/>
+                    <IconButton>
+                        <SearchIcon className='text-gray-400'/>
+                    </IconButton>
                 </div>
             </div>
 
             {/* header right */}
-            <ShoppingCartOutlinedIcon />
-            {/*32:59*/}
+            <IconButton>
+                <ShoppingCartOutlinedIcon />
+            </IconButton>
+            {/*25:40*/}
         </header>
     )
 }
